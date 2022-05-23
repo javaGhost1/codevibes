bf5d4a70904ef7cc35d2817968659a464c1c0d7c
javaGhost <dennismurage97@gmail.com>
2022-05-23 19:21:40 +0300

    added requirements file

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = 'blog'
 urlpatterns = [
     path("", views.BlogListView.as_view(), name='blog_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:blog>/', views.blog_detail, name='blog_detail'),
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
