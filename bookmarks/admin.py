7f4863b3d184ce853aba592fded38fd8eda889e4
JavaGhost1 <Secret09!>
2023-10-02 16:04:13 +0300

    final styling updates

--- bookmarks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-@admin.register(Bookmark)
-class BookmarkAdmin(admin.ModelAdmin):
-    list_display = ['blog']
-    
